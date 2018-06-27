caf32bb7b181869efa2ad88e92ce98951b2d02d8
Seungwoo Park <spark@colubrismx.priv>
2018-06-27 16:15:41 -0500

    Test4

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,6 @@
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, World!\n"); //My mistake
-   return 0;
+   printf("Hello, World! Seungwoo\n")
+      return 0;
 }
