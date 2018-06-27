a6f0628d2edf47a24bce21c980d8d5aa4cd2eeda
COLUBRISMX\spark <peter.pakr@colubrismx.com>
2018-06-26 20:39:19 -0500

    fised mistake

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,6 @@
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, World! Hello, Seungwoo! Hello Jenkins!!\n"); My mistake
+   printf("Hello, World! Hello, Seungwoo! Hello Jenkins!!\n"); //My mistake
    return 0;
 }
