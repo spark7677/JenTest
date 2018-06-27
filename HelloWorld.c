e243f4d590b5127a67bd6a8440d5e3c868418bf0
COLUBRISMX\spark <peter.pakr@colubrismx.com>
2018-06-26 20:22:14 -0500

    real!!

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,5 @@
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, World! Hello, Seungwoo! Hello Jenkins!!"\n);
+   printf("Hello, World! Hello, Seungwoo! Hello Jenkins!!\n");
    return 0;
-}
