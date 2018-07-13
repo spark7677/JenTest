657861d8a21703937d176d6619bb70c3379f11d0
COLUBRISMX\spark <peter.pakr@colubrismx.com>
2018-07-12 20:09:53 -0500

    shell

---
+++ Deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello this is shell"
+
+cppcheck HelloWorld.c main.c simple.c --xml 2> cppcheck-result.xml
+
+gcc -o simple.exe simple.c
+gcc -o Hello.exe HelloWorld.c main.c
+
+./simple.exe
+./Hello.exe
