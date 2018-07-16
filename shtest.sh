df037fb9e586d6b5e94ac57820d0dcca9172aa9b
EC2 Default User <ec2-user@ip-172-31-23-174.us-west-2.compute.internal>
2018-07-16 13:59:31 +0000

    EC2 Cron

---
+++ shtest.sh
@@ -0,0 +1,13 @@
+echo "Build Step"
+
+git pull
+
+cppcheck . --xml 2> cppcheck-result.xml
+gcc -c HelloWorld.c main.c
+gcc -o Hello HelloWorld.o main.o
+./Hello
+
+git add .
+
+git commit -m "EC2 Cron"
+git push
